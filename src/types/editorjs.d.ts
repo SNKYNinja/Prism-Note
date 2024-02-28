c6ee34e4a34909dcf521ea66e30fe26c40afb803
Aditya Chaudhary <96983394+SNKYNinja@users.noreply.github.com>
2024-02-29 01:56:32 +0530

    feat: Add declaration file for editorjs tools

---
+++ src/types/editorjs.d.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// header.d.ts
+declare module "@editorjs/header" {
+    const Header: any;
+    export = Header;
+}
+
+// embed.d.ts
+declare module "@editorjs/embed" {
+    const Embed: any;
+    export = Embed;
+}
+
+// table.d.ts
+declare module "@editorjs/table" {
+    const Table: any;
+    export = Table;
+}
+
+// list.d.ts
+declare module "@editorjs/list" {
+    const List: any;
+    export = List;
+}
+
+// code.d.ts
+declare module "@editorjs/code" {
+    const Code: any;
+    export = Code;
+}
+
+// link.d.ts
+declare module "@editorjs/link" {
+    const LinkTool: any;
+    export = LinkTool;
+}
+
+// inline-code.d.ts
+declare module "@editorjs/inline-code" {
+    const InlineCode: any;
+    export = InlineCode;
+}

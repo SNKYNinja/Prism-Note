cc46d5899931c9efea5d50fceb8fdfbd61f99e91
Aditya Chaudhary <96983394+SNKYNinja@users.noreply.github.com>
2024-02-29 21:32:32 +0530

    feat: Add context for singular note

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,33 +2,44 @@ import { Note } from "@/types/type";
 import Star from "@/components/star";
 import CardSkeleton from "@/components/card-skeleton";
 import { Button } from "@/components/ui/button";
-import Editor from "@/components/editor";
+import EditorDialog from "@/components/editor-dialog";
+import { createContext, useState } from "react";
+
+interface NoteContextData {
+    note: Note;
+    setNote: React.Dispatch<React.SetStateAction<Note>>;
+}
+
+export const NoteContext = createContext<NoteContextData | null>(null);
 
 export default function Card({
-    note,
+    noteData,
     isLoading,
 }: {
-    note: Note;
+    noteData: Note;
     isLoading: boolean;
 }) {
-    const { id, title, tag, timeCreated } = note;
+    const [note, setNote] = useState<Note>(noteData);
+    const { title, tag, timeCreated } = note;
 
     if (isLoading) return <CardSkeleton />;
     return (
-        <div
-            className={`bg-${tag}-300 col-span-1 shadow-md rounded-xl p-4 h-64 drop-shadow-md group`}
-        >
-            <div className="relative size-full">
-                <header className="text-2xl max-w-[70%]">{title}</header>
-                <Button
-                    variant="ghost"
-                    className="rounded-full p-2 bg-transparent hover:bg-transparent absolute top-0 right-0"
-                >
-                    <Star width="1.5rem" height="1.5rem" noteId={id} />
-                </Button>
-                <p className="opacity-70 select-none">{timeCreated}</p>
-                <Editor />
+        <NoteContext.Provider value={{ note, setNote }}>
+            <div
+                className={`bg-${tag}-300 col-span-1 shadow-md rounded-xl p-4 h-64 drop-shadow-md group`}
+            >
+                <div className="relative size-full">
+                    <header className="text-2xl max-w-[70%]">{title}</header>
+                    <Button
+                        variant="ghost"
+                        className="rounded-full outline-none p-2 bg-transparent hover:bg-transparent absolute top-0 right-0"
+                    >
+                        <Star width="1.5rem" height="1.5rem" />
+                    </Button>
+                    <p className="opacity-70 select-none">{timeCreated}</p>
+                    <EditorDialog />
+                </div>
             </div>
-        </div>
+        </NoteContext.Provider>
     );
 }

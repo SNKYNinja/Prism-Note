056890e8ee8d402db0b38b859d1f9cc03d5465fd
Aditya Chaudhary <96983394+SNKYNinja@users.noreply.github.com>
2024-03-04 20:47:34 +0530

    feat: Add getCurrentTime function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,6 +10,23 @@ export function capitalize(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+export function getCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = now.getFullYear();
+
+    return `${hours}:${minutes}, ${day} ${month} ${year}`;
+}
+
+export function generateID() {
+    const rand = Math.floor(Math.random() * 1000000000);
+    console.log(rand);
+    return rand;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function setLocalStorage(value: Note[]) {
     localStorage.setItem("notes", JSON.stringify(value));
